e497bfc3bd756d29935ff0d5759e26438fd56741
skywalkerDarren <yh322yh@outlook.com>
2020-11-11 18:18:03 +0800

    upgrade project and migrate to androidX

--- sample/src/main/java/com/ethanhua/skeleton/sample/MainActivity.java
+++ sample/src/main/java/com/ethanhua/skeleton/sample/MainActivity.java
@@ -2,8 +2,8 @@ package com.ethanhua.skeleton.sample;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
